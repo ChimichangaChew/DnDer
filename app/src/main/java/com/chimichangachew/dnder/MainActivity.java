656f570ed14f90be2e720691b90917e97c919059
Guy444 <Guyxert@gmail.com>
2020-10-11 13:53:40 -0500

    Added a log in button and started working on login logic

--- app/src/main/java/com/chimichangachew/dnder/MainActivity.java
+++ app/src/main/java/com/chimichangachew/dnder/MainActivity.java
@@ -3,12 +3,16 @@ package com.chimichangachew.dnder;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText meditTextPassword = findViewById(R.id.editTextTextPassword);
+    EditText meditTextUser = findViewById(R.id.editTextTextUser);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login);
     }
 }
